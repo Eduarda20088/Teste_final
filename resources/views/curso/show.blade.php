1448a613b19668c50c8ac93511fedcbdde5b57ea
Maria Eduarda <dudapedreira2008@gmail.com>
2025-10-21 21:48:08 -0300

    Views removidas

--- resources/views/curso/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-@section('title', 'Visualizar Curso')
-@section('content')
-<div class="container mt-4">
-    <h1>Detalhes do Curso</h1>
-    <p><strong>Nome:</strong> {{ $curso->nome }}</p>
-    <a href="{{ route('curso.index') }}" class="btn btn-primary mt-3">Voltar</a>
-</div>
-@endsection
